511b228e20f5f7efda416a929e536fff4a9d6835
Marcin Dziedzinski <m.m.dziedzinski@gmail.com>
2023-01-31 09:57:50 +0100

    not fixed

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -53,7 +53,6 @@ const Video = (props: Props) => {
                   autoPlay
                   muted
                   loop
-                  controls
                   id="myVideo"
                 >
                   <source src="../assets/gb_wideo.mp4" type="video/mp4" />
